d1777b3b4a1d886b7d8d259671127ac4e665d48c
AdrianC <aacuellar14@gmail.com>
2022-05-29 21:52:41 -0700

    basic gui

--- javascript.js
+++ javascript.js
@@ -2,26 +2,18 @@ const memory = document.querySelector('#Memory');
 const containerB = document.querySelector('#Body');
 let decPress=false;
 
-theSlider.oninput = function() {
-    while(containerB.lastChild){
-        containerB.removeChild(containerB.lastChild);
-    }
-    gridDimension=parseInt(this.value);
-}
-
-
 const buttons = document.querySelectorAll('button');
 
 
 buttons.forEach((button) => {
 
     button.addEventListener('click', () => {
-        if (button.id !="backspace" && button.id !="clear" && button.id !="equals" && button.id !="decimal"){
+        if (button.id !="backspace" && button.id !="CE" && button.id !="equals" && button.id !="decimal"){
             memory.textContent+=button.id;
         }
         else if(button.id == "backspace")
             memory.textContent =memory.textContent.substring(0,memory.textContent.length-1);
-        else if(button.id == "clear")
+        else if(button.id == "CE")
             memory.textContent = "";
         else if(button.id == "equals"){
             
@@ -31,8 +23,8 @@ buttons.forEach((button) => {
                 console.log("No good");
             else{
                 decPress=true;
+                memory.textContent+=button.id;
             }
-
         }
     });
 
