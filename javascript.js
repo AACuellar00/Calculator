90fe1475ecba5b2650e8aed867ed024fc3ff781b
AdrianC <aacuellar14@gmail.com>
2022-05-30 14:11:52 -0700

    operations complete

--- javascript.js
+++ javascript.js
@@ -5,38 +5,146 @@ let decPress=false;
 let lastPressNum=false;
 let pressedNum=false;
 let operPressed=false;
-let prevNum=0;
-let currentNum=0;
+let prevNum="0";
+let currentNum="0";
+let lastOpp="";
 
 const numbers = document.getElementsByClassName("num");
 const operators = document.getElementsByClassName("operator");
 const dels = document.getElementsByClassName("del");
+const finish = document.getElementById("equals");
 
 function operAdd(){
+    let result =parseInt(prevNum) + parseInt(currentNum);
+    let dec=0;
 
+    if (Math.floor(result) !== result) {
+        dec=result.toString().split(".")[1].length || 0;
+    }
+    if(dec>=10)
+        result=result.toFixed(10);
+
+    memory.textContent=result;
+    prevNum="0";
+    currentNum=result;
+    lastOpp="";
 }
+
 function operSub(){
+    let result =parseInt(prevNum) - parseInt(currentNum);
+    let dec=0;
 
+    if (Math.floor(result) !== result) {
+        dec=result.toString().split(".")[1].length || 0;
+    }
+    if(dec>=10)
+        result=result.toFixed(10);
+
+    memory.textContent=result;
+    prevNum="0";
+    currentNum=result;
+    lastOpp="";
 }
+
 function operMulti(){
+    let result =parseInt(prevNum) * parseInt(currentNum);
+    let dec=0;
 
+    if (Math.floor(result) !== result) {
+        dec=result.toString().split(".")[1].length || 0;
+    }
+    if(dec>=10)
+        result=result.toFixed(10);
+
+    memory.textContent=result;
+    prevNum="0";
+    currentNum=result;
+    lastOpp="";
 }
+
 function operDiv(){
-
+    if(parseInt(currentNum)==0){
+        alert("Please do not divide by 0");
+    }
+    else{
+        let result =parseInt(prevNum) / parseInt(currentNum);
+        let dec=0;
+    
+        if (Math.floor(result) !== result) {
+            dec=result.toString().split(".")[1].length || 0;
+        }
+        if(dec>=10)
+            result=result.toFixed(10);
+    
+        memory.textContent=result;
+        prevNum="0";
+        currentNum=result;
+        lastOpp="";
+    }
 }
+
 function operModu(){
+    let result =parseInt(prevNum) % parseInt(currentNum);
+    let dec=0;
 
+    if (Math.floor(result) !== result) {
+        dec=result.toString().split(".")[1].length || 0;
+    }
+    if(dec>=10)
+        result=result.toFixed(10);
+
+    memory.textContent=result;
+    prevNum="0";
+    currentNum=result;
+    lastOpp="";
 }
+
 function operExpo(){
+    let result =parseInt(prevNum) ^ parseInt(currentNum);
+    let dec=0;
 
+    if (Math.floor(result) !== result) {
+        dec=result.toString().split(".")[1].length || 0;
+    }
+    if(dec>=10)
+        result=result.toFixed(10);
+
+    memory.textContent=result;
+    prevNum="0";
+    currentNum=result;
+    lastOpp="";
 }
-function operate(){
 
+function operate(){
+    switch(lastOpp){
+        case "+":
+            operAdd();
+            break;
+        case "-":
+            operSub();
+            break;
+        case "*":
+            operMulti();
+            break;
+        case "/":
+            operDiv();
+            break;
+        case "%":
+            operModu();
+            break;
+        case "^":
+            operExpo();
+            break;
+    }
 }
 
 function backspace(){
-//modify all bools and such based on last character
+    let lastChar = memory.textContent.charAt(memory.textContent.length-1);
+    let secondLastChar = memory.textContent.charAt(memory.textContent.length-2);
+    
+
     memory.textContent =memory.textContent.substring(0,memory.textContent.length-1);
+
     if(memory.textContent.length==0){
         memory.textContent="0";
         decPress=false;
@@ -44,7 +152,8 @@ function backspace(){
         prevNum=0;
         pressedNum=false;
         operPressed=false;
-}
+        lastOpp="";
+    }
 }
 
 function CE(){
@@ -54,20 +163,23 @@ function CE(){
     prevNum=0;
     pressedNum=false;
     operPressed=false;
+    lastOpp="";
 }
 
 
 for (const num of numbers){
     num.addEventListener('click',() =>{
         if(memory.textContent=="0"){
-                memory.textContent=num.id;
+            memory.textContent=num.id;
+            pressedNum=true;
+            lastPressNum=true;
         }
         else{
             memory.textContent+=num.id;
             lastPressNum=true;
             pressedNum=true;
-            
             operPressed=false;
+            currentNum=memory.textContent;
         }
     });
 }
@@ -77,12 +189,15 @@ for (const oper of operators){
         if(pressedNum==true){
             prevNum=parseInt(memory.textContent);
             memory.textContent+=oper.id;
+            currentNum="";
+            lastOpp=oper.id;
             pressedNum==false;
             operPressed=true;
             lastPressNum=false;
             decPress=false;
         }
         else{
+            
         }
     });
 }
@@ -90,73 +205,26 @@ for (const oper of operators){
 for (const del of dels){
     del.addEventListener('click', () =>{
         if(del.id=="CE"){
-            
+            CE();
         }
         else{
-            
+            backspace();
         }
     });
 }
 
+equals.addEventListener('click', () =>{
+    if(lastPressNum==true && operPressed==true){
+        operate();
+    }
+    else{
 
-
-
-
-
-
-/*
-buttons.forEach((button) => {
-
-    button.addEventListener('click', () => {
-        if (button.id !="backspace" && button.id !="CE" && button.id !="equals" && button.id !="."){
-
-            if(memory.textContent=="0")
-                memory.textContent=button.id;
-            else if(button.id =="+" || button.id =="-" || button.id =="*" || button.id =="/"){
-                if(lastPressNum){
-                    memory.textContent+=button.id;
-                    lastPressNum=false;
-                }
-                else
-                    console.log("No good");
-            }
-                
-            else{
-                memory.textContent+=button.id;
-                lastPressNum=true;
-            }
-        }
-
-
-        //logic of lastpressnum for CE/Backspace
-
-        else if(button.id == "backspace"){
-            memory.textContent =memory.textContent.substring(0,memory.textContent.length-1);
-            if(memory.textContent.length==0)
-                memory.textContent="0";
-        }
-
-        else if(button.id == "CE")
-            memory.textContent = "0";
-        else if(button.id == "equals"){
-            
-        }
-
-        else{
-            if(decPress==true || lastPressNum==false){
-                console.log("No good");
-            }
-            else{
-                decPress=true;
-                lastPressNum=false;
-                memory.textContent+=button.id;
-            }
-        }
-    });
-
+    }
 });
 
 
 
-*/
+
+
+
 
